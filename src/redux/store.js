a8700dcb024f60babaf0878b0dd69ffea94a539a
Tizazab <tizazabayana2@gmail.com>
2025-05-07 05:09:26 -0700

    added productReducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/auth.slice";
+import productReducer from "./slices/product.slice";
 // congigure store
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        product: productReducer,
     },
 });
 
