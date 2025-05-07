48acfac55fbe190791927c6c738f0d1a6a40bf81
Tizazab <tizazabayana2@gmail.com>
2025-05-07 09:58:16 -0700

    edited product asyncthunk functions

--- src/redux/slices/cart.slice.js
+++ src/redux/slices/cart.slice.js
@@ -1,4 +1,4 @@
-import { createSlice , createAsyncThunk} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // Base URL
@@ -8,7 +8,7 @@ const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 // Helper function to get cart items from local storage
 const loadCartFromLocalStorage = () => {
     const cart = localStorage.getItem("cart");
-    return cart ? JSON.parse(cart) : { products: [],};
+    return cart ? JSON.parse(cart) : { products: [], };
 }
 
 
@@ -19,9 +19,9 @@ const saveCartToLocalStorage = (cart) => {
 
 
 // Async thunk to fetch cart for a user or guest
-export const fetchCart = createAsyncThunk("cart/fetchCart" , async ({userId , guestId} , { rejectWithValue }) =>{
+export const fetchCart = createAsyncThunk("cart/fetchCart", async ({ userId, guestId }, { rejectWithValue }) => {
     try {
-        const response = await axios.get(`${BASE_URL}/api/carts` , 
+        const response = await axios.get(`${BASE_URL}/api/carts`,
             {
                 params: {
                     userId,
@@ -31,14 +31,14 @@ export const fetchCart = createAsyncThunk("cart/fetchCart" , async ({userId , gu
         );
         return response.data.cart;
     } catch (error) {
-       return rejectWithValue(error.response.data);
+        return rejectWithValue(error.response.data);
     }
 });
 
 
 // Async thunk to add a product to the cart
-export const addToCart = createAsyncThunk("cart/addToCart", async ({ productId, quantity, size, color, guestId, userId}, { rejectWithValue }) => {
-    
+export const addToCart = createAsyncThunk("cart/addToCart", async ({ productId, quantity, size, color, guestId, userId }, { rejectWithValue }) => {
+
     try {
         const response = await axios.post(`${BASE_URL}/api/carts`,
             {
@@ -53,6 +53,72 @@ export const addToCart = createAsyncThunk("cart/addToCart", async ({ productId, 
 
         return response.data.cart;
     } catch (error) {
-       return rejectWithValue(error.response.data); 
+        return rejectWithValue(error.response.data);
+    }
+});
+
+
+// Async thunk to update the quantity of a product in the cart
+export const updateCartQuantity = createAsyncThunk("cart/updateCartQuantity", async ({ productId, quantity, size, color, guestId, userId }, { rejectWithValue }) => {
+
+    try {
+        const response = await axios.put(`${BASE_URL}/api/carts`,
+            {
+                productId,
+                quantity,
+                size,
+                color,
+                guestId,
+                userId,
+            }
+        );
+
+        return response.data.cart;
+    } catch (error) {
+        return rejectWithValue(error.response.data);
+    }
+});
+
+
+// Async thunk to remove a product from the cart
+export const removeFromCart = createAsyncThunk("cart/removeFromCart", async ({ productId, size, color, guestId, userId }, { rejectWithValue }) => {
+
+    try {
+        const response = await axios.delete(`${BASE_URL}/api/carts`,
+            {
+                productId,
+                size,
+                color,
+                guestId,
+                userId,
+            }
+        );
+
+        return response.data.cart;
+    } catch (error) {
+        return rejectWithValue(error.response.data);
+    }
+});
+
+
+// Async thunk to merge guest cart with user cart
+export const mergeGuestCart = createAsyncThunk("cart/mergeGuestCart", async ({ guestId }, { rejectWithValue }) => {
+    
+    try {
+       const response = await axios.post(`${BASE_URL}/api/carts/merge`,
+            {
+                guestId,
+            },
+
+            {
+                headers:{
+                    Authorization : `Bearer ${localStorage.getItem("token")}`,
+                }
+            }
+        );
+
+        return response.data.cart; 
+    } catch (error) {
+        return rejectWithValue(error.response.data);
     }
 })
