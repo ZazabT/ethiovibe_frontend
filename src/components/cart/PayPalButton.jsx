d7b31144d9d702b5b0739e385855c77761a08b44
Tizazab <tizazabayana2@gmail.com>
2025-05-12 08:26:53 -0700

    refactor(cart): move PayPal client ID to environment variable
    The PayPal client ID was previously hardcoded in the PayPalButton component. This commit refactors the code to fetch the client ID from an environment variable, improving security and maintainability. The code formatting was also cleaned up for better readability.

--- src/components/cart/PayPalButton.jsx
+++ src/components/cart/PayPalButton.jsx
@@ -1,25 +1,25 @@
-import { PayPalButtons , PayPalScriptProvider } from "@paypal/react-paypal-js"
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"
 
-const PayPalButton = ({amount , onSuccess , onError}) => {
+const PayPalButton = ({amount, onSuccess, onError}) => {
+   const clientId = import.meta.env.VITE_PAYPAL_CLIENT_ID;
+   
    return (
-      <PayPalScriptProvider options={{ "client-id": "AWUMX1QVL3G-1L2tMfKu7pu-y3pd-hT-b4E3mdaImdzcWHvBNkuivIAiEeiJX7PE8vOycdDB36zu5iQG" }}>
-         <PayPalButtons style={{layout: "vertical"}}
-         createOrder={(data, actions) => {
-            return actions.order.create({
-               purchase_units: [{
-                  amount: {
-                     value: amount
-                  }
-               }]
-            }) 
-         }}
-
-         onApprove={(data,actions) =>{
-            return actions.order.capture().then(onSuccess)
-         }}
-
-         onError={onError}
-         
+      <PayPalScriptProvider options={{ "client-id": clientId }}>
+         <PayPalButtons 
+            style={{layout: "vertical"}}
+            createOrder={(data, actions) => {
+               return actions.order.create({
+                  purchase_units: [{
+                     amount: {
+                        value: amount
+                     }
+                  }]
+               }) 
+            }}
+            onApprove={(data,actions) => {
+               return actions.order.capture().then(onSuccess)
+            }}
+            onError={onError}
          />
       </PayPalScriptProvider>
    )
