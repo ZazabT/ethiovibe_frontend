0b0d26f6e3ff77d140232bcbb267ec51b445aa31
Tizazab <tizazabayana2@gmail.com>
2025-05-12 01:47:31 -0700

    User checkout slice on the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/auth.slice";
 import productReducer from "./slices/product.slice";
 import cartReducer from "./slices/cart.slice";
+import checkOutReducer from './slices/checkout.slice'
 // congigure store
 const store = configureStore({
     reducer: {
         auth: authReducer,
         product: productReducer,
         cart: cartReducer,
+        checkOut: authReducer
     },
 });
 
