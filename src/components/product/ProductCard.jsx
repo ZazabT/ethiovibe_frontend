1d5f432d1f43ec03fda205496c80bdcf12ca005e
Tizazab <tizazabayana2@gmail.com>
2025-05-20 07:01:59 -0700

    fix(product): ensure discountPercentage is greater than 0 before rendering
    This change prevents rendering the discount badge when the discountPercentage is 0 or undefined, ensuring the UI only displays relevant discount information.

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -17,11 +17,11 @@ const ProductCard = ({ product }) => {
                 alt={product.name}
                 className="w-full h-[400px] object-cover transition-transform duration-500 group-hover:scale-110"
               />
-              {product.discountPercentage && (
-              <div className="absolute top-4 left-0 bg-pink-400 text-white px-4 py-1 text-sm font-medium">
-                {product.discountPercentage}% OFF
-              </div>
-            )}
+              {product.discountPercentage > 0 && (
+                <div className="absolute top-4 left-0 bg-pink-400 text-white px-4 py-1 text-sm font-medium">
+                  {product.discountPercentage}% OFF
+                </div>
+              )}
             </div>
             <div className="space-y-2 pb-4">
               <h3 className="text-lg font-medium exo-font truncate">{product.name}</h3>
